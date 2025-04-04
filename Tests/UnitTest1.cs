3c6864cdb57e10e1d1a2341fc1b37043d4915e38
Ghestr <malosalexandru2004@gmail.com>
2025-04-04 16:44:49 +0300

    Created test project
    need to add references and shit

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int a = 5;
+            Assert.AreEqual(5, a);
+        }
+    }
+}
