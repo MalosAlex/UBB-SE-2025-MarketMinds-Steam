e9ff77346d2b48fb5bf934dd92d536b5942181ba
CostinCJ <147099173+CostinCJ@users.noreply.github.com>
2025-04-10 14:25:52 +0300

    Fixed points buying error

--- BusinessLayer/Data/Procedures/Walllet/BuyPoints.sql
+++ BusinessLayer/Data/Procedures/Walllet/BuyPoints.sql
@@ -3,9 +3,9 @@ as
 begin
 	update Wallet 
 	set points = points + @numberOfPoints
-	where user_id = @userId;
+	where user_id = @user_Id;
 
 	update Wallet
 	set money_for_games = money_for_games - @price 
-	where user_id = @userId
+	where user_id = @user_Id
 end
