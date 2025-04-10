b66d23eeb263bf404036a5b198f589561955acf7
CostinCJ <147099173+CostinCJ@users.noreply.github.com>
2025-04-10 14:12:20 +0300

    Fixed wallet crashing

--- BusinessLayer/Data/Procedures/Walllet/AddMoney.sql
+++ BusinessLayer/Data/Procedures/Walllet/AddMoney.sql
@@ -1,6 +1,6 @@
-create or alter procedure AddMoney @amount decimal, @userId int as
+create or alter procedure AddMoney @amount decimal, @user_Id int as
 begin 
 	update wallet  
 	set money_for_games = money_for_games + @amount
-	where user_id = @userId
+	where user_id = @user_Id
 end
